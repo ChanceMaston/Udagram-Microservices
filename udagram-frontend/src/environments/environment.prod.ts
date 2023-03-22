9816169c88c547a82efb8135584e471ccb26b4ca
chance.maston <chance.maston@cradlepoint.com>
2023-03-22 16:54:46 -0600

    Project Done

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab7fc5128148641ff801b160aac4f540-264798099.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
